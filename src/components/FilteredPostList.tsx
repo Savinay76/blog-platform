5fe22be8e5fd88fa3272ee13b296603e6eb8f67f
Savinay76 <savinayreddysathi172@gmail.com>
2026-02-14 16:10:53 +0530

    Fix PostCard import in FilteredPostList again

--- src/components/FilteredPostList.tsx
+++ src/components/FilteredPostList.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { PostData } from '@/lib/types';
 import Search from '@/components/Search';
+import PostCard from '@/components/PostCard';
 import Pagination from '@/components/Pagination';
 
 interface FilteredPostListProps {
